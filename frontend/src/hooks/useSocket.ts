bc8101f06b46ad2706996252fbe4efb551a5f16f
Dhawal Sarode <dhawalsarode.ai@gmail.com>
2025-12-23 00:31:33 +0530

    fix: socket.io production URL

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -3,26 +3,34 @@ import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
 
+const SOCKET_URL = import.meta.env.PROD
+  ? "https://collaborative-task-manager-backend-gve1.onrender.com"
+  : "http://localhost:5000";
+
 export const useSocket = (
-  onCreated: (task: any) => void,
-  onUpdated: (task: any) => void,
-  onDeleted: (taskId: string) => void
+  onCreated?: (task: any) => void,
+  onUpdated?: (task: any) => void,
+  onDeleted?: (taskId: string) => void
 ) => {
   useEffect(() => {
     if (!socket) {
-      socket = io("http://localhost:5000", {
-        withCredentials: true
+      socket = io(SOCKET_URL, {
+        withCredentials: true,
+        transports: ["websocket"]
       });
     }
 
-    socket.on("task:created", onCreated);
-    socket.on("task:updated", onUpdated);
-    socket.on("task:deleted", ({ taskId }) => onDeleted(taskId));
+    if (onCreated) socket.on("task:created", onCreated);
+    if (onUpdated) socket.on("task:updated", onUpdated);
+    if (onDeleted)
+      socket.on("task:deleted", ({ taskId }) => onDeleted(taskId));
 
     return () => {
-      socket?.off("task:created", onCreated);
-      socket?.off("task:updated", onUpdated);
-      socket?.off("task:deleted");
+      if (!socket) return;
+
+      if (onCreated) socket.off("task:created", onCreated);
+      if (onUpdated) socket.off("task:updated", onUpdated);
+      if (onDeleted) socket.off("task:deleted");
     };
   }, []);
 };
